13314ec5bf5fca7e37ed7c711dfdc7f673bc6d89
shravani0804 <sonarshravani2005@gmail.com>
2025-03-10 20:19:56 +0530

    register.php

--- admin/register.php
+++ admin/register.php
@@ -1,6 +1,50 @@
 <?php
 include '../components/connect.php';
-?>
+
+if(isset($_POST['register'])){
+
+    $id=unique_id();
+    $name=$_POST['name'];
+    $name=$filter_var($name, FILTER_SANITIZE_STRING);
+
+    $email=$_POST['email'];
+    $email=$filter_var($email, FILTER_SANITIZE_STRING);
+
+    $pass=$_POST['pass'];
+    $pass=$filter_var($pass, FILTER_SANITIZE_STRING);
+
+    
+    $cpass=$_POST['cpass'];
+    $cpass=$filter_var($cpass, FILTER_SANITIZE_STRING);
+
+    $image=$_FILES['image']['name'];
+    $image=$filter_var($image, FILTER_SANITIZE_STRING);
+    $ext= pathinfo($image, PATHINFO_EXTENSION);
+    $rename=unique_id().'.'.$ext;
+    $image_size=$_FILES['image']['size'];
+    $image_tmp_name=$_FILES['image']['tmp_name'];
+    $image_folder='../uploaded_files'.$rename;
+
+    $select_seller=$conn->prepare("SELECT*FROM'sellers'WHERE email=?");
+    $select_seller->execute([$email]);
+
+    if($select_seller->rowCount()>0){
+        $warning_msg[] = 'email already exist';
+    }
+    else{
+        if($pass != $cpass){
+            $warning_msg[] = 'confirm passwprd does not matched';
+        }else{
+            $insert_seller = $conn->prepare("INSERT INTO 'sellers' (id, name, email, password,image) VALUES(?,?,?,?,?)");
+            $insert_seller->execute([$id, $name, $email, $cpass, $rename]);
+            move_uploaded_file($image_tmp_name, $image_folder);
+            $success_msg[]='new seller registered! please login now';
+        }
+
+        }
+    }
+
+  ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -14,7 +58,8 @@ include '../components/connect.php';
 </head>
 <body>
     <div class="form-container">
-        <form action="" method="posy" accept="multipart/form-data" class="register">
+        <form action="" method="post" enctype="multipart/form-data" class="register">
+            <h3>Register Now</h3>
             <div class="flex">
                 <div class="col">
                 <div class="input-field">
@@ -41,6 +86,7 @@ include '../components/connect.php';
                         <p>Select profile <span>*</span></p>
                         <input type="file" name="image" accept="image/*" required-class="box">
             </div>
+            <p class="link">Already have an account? <a href="login.php">Login now</a></p>
             <input type="submit" name="register" class="btn" value="register now"> 
                     
         </form> 
